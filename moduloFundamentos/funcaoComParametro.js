77240de283b6584ce128f3c1e792e6d7f1861daf
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-27 00:47:19 -0300

    function com parametros

---
+++ moduloFundamentos/funcaoComParametro.js
@@ -0,0 +1,13 @@
+//Funçao sendo passado paramentros
+
+function funcaoComSoma(valor1, valor2){
+    console.log(valor1 + valor2)
+}
+
+function funcaoComParametro(valor1, valor2){
+    soma = valor1 + valor2
+    return soma
+}
+
+funcaoComSoma(6,6)
+console.log(funcaoComParametro(3,6))
