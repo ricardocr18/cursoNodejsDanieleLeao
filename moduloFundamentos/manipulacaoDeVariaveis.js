63af245b0a2f39a18383ac5187ed9f81154b6876
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-29 00:27:30 -0300

    manipulador de variaveis

---
+++ moduloFundamentos/manipulacaoDeVariaveis.js
@@ -0,0 +1,10 @@
+//objeto criado com const
+const usuario ={
+    name: "ricardo",
+    idade: 46
+}
+
+//Eu só consigo mudar um valor no objeto criado por Const, após indicar o que quero mudar no objeto. Como a linha de baixo
+usuario.name = "Raquel"
+
+console.log(usuario)
