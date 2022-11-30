c263c6f24ff27df1919ef62f0b1b573fe7652d4c
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-30 12:55:20 -0300

    exemplo de for

---
+++ moduloFundamentos/for.js
@@ -0,0 +1,5 @@
+// FOR (variavel=inicio; fim ; incremento) o incremento pode ser ++ ou +=
+
+for(var variavel = 0; variavel <= 10; variavel ++){
+    console.log('O valor da variavel é ' + variavel);
+}
