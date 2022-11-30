2dfb61c9d265fb36e3fee7d2440c17391814ac25
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-30 12:42:12 -0300

    condicionais if/else/switch

---
+++ moduloFundamentos/condicionais.js
@@ -0,0 +1,23 @@
+/**
+ * IF
+ * ELSE
+ * ELSE IF
+ * SWITCH estamos nesse exemplo só usando esse caso mesmo
+ */
+
+let mes = 7;
+
+switch(mes){
+    case 1:
+        console.log('O mÊs é Janeiro')
+        break;
+    case 2:
+        console.log('O mÊs é Fevereiro')
+        break;
+    case 3:
+        console.log('O mÊs é Março')
+        break;
+    default:
+        console.log("O mÊs é qualquer um")
+        break;
+}
