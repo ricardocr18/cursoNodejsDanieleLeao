be5a0e0de902309bd4f5afcaf4a00fbb10c99d12
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-12-07 00:23:21 -0300

    exemplo de fincoes-array

---
+++ moduloFundamentos/funcoes-array.js
@@ -0,0 +1,15 @@
+/** OUTROS FUNÇÕES QUE PODEMOS USAR
+ * 
+ * POP - Remove o ultimo item do array
+ * SHIFT - Remove o primeiro item do array
+ * UNSHIFT - Adicionar no início do Array 
+ * 
+ */
+
+
+
+const nomes = ['Marias', 'Ricardo', 'Zenilda', 'Tereza', 'Jorge', 'Fernando']
+
+//INDEXOF - Encontra o index do item
+const item = nomes.indexOf('Fernando');
+console.log("item encontrado " + item)
