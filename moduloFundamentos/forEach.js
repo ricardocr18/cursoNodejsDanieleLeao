bfd304f370de7efb7852d22bef4db7312ef785eb
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-12-07 23:27:49 -0300

    exemplo de forEach

---
+++ moduloFundamentos/forEach.js
@@ -0,0 +1,7 @@
+
+const nomes = ['Marias', 'Ricardo', 'Zenilda', 'Tereza', 'Jorge', 'Fernando']
+nomes.forEach((nome) => console.log('o nome é ' + nome))
+
+for (var index = 0; index < nomes.length; index++){
+    console.log('Utilizando o for, o nome é ' + nomes[index])
+}
