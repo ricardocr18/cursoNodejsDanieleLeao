00de09dac0c461d654b31f0d81cb21038763da8d
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-12-09 17:56:58 -0300

    retotno em JSON

--- modulo-api/index.js
+++ modulo-api/index.js
@@ -1,9 +1,16 @@
 const http = require("http");
 const server = http.createServer((request, response)=> {
-    response.write("minha primeira resposta")
+
+    const result = {
+        message: "minha primeira resposta"
+    }
+
+        response.writeHead(201, {
+            'Content-type': 'application/json'
+        })
+
+    response.write(JSON.stringify(result))
     response.end()
 });
-
-
 
 server.listen(3000, () => console.log("Servidor está no AR"))
