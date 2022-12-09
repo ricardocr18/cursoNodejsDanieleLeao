aed1c5ee32f101b0b446fd789a03c086a7488d5e
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-12-09 17:32:25 -0300

    criando primeira resposta

--- modulo-api/index.js
+++ modulo-api/index.js
@@ -1,4 +1,9 @@
 const http = require("http");
-const server = http.createServer((request, response)=> {});
+const server = http.createServer((request, response)=> {
+    response.write("minha primeira resposta")
+    response.end()
+});
+
+
 
 server.listen(3000, () => console.log("Servidor está no AR"))
