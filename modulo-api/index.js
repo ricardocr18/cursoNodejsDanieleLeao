b283a0fd23ef5b760fe89040a30b9a600e212500
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-12-10 21:15:50 -0300

    criando método POST

--- modulo-api/index.js
+++ modulo-api/index.js
@@ -1,16 +1,34 @@
 const http = require("http");
-const server = http.createServer((request, response)=> {
+const { randomUUID } = require("crypto"); //Aqui criei uma função para usar na criação de numeros randomicos
 
-    const result = {
-        message: "minha primeira resposta"
+
+
+let users = []
+const server = http.createServer((request, response)=> {
+    //cadastro de usuário
+
+    const METHOD = request.method;
+    if(METHOD === 'POST'){
+
+        request.on("data", (data) => {
+            const body = JSON.parse(data);
+            const user = {
+                ...body,
+                id: randomUUID()
+            }
+            users.push(user)
+            return response.end(JSON.stringify(user))
+        })
+
     }
 
-    response.statusCode = 201;
-    response.setHeader("Content-type", "aplication/json");
-    response.statusMessage = "Usuário salvo com sucesso"
-        
-    response.write(JSON.stringify(result))
-    response.end()
+    //GET - Listar todods os usuário
+    //POS - Inseriri os usuário
+    //PUT - Alterar um usuário
+    //Delete - Remover um usuário
+
+
+
 });
 
 server.listen(3000, () => console.log("Servidor está no AR"))
