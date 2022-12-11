fbc08a6ecdbcea6c7117e64351e43101d03ee47c
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-12-11 00:40:39 -0300

    criando método Put

--- modulo-api/index.js
+++ modulo-api/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const { randomUUID } = require("crypto"); //Aqui criei uma função para usar na criação de numeros randomicos
 
-//GET - Listar todods os usuário
+
 //PUT - Alterar um usuário
 //Delete - Remover um usuário
 
@@ -12,9 +12,8 @@ const server = http.createServer((request, response) => {
     const METHOD = request.method;
     const URL = request.url
 
-    if (URL === "/users") {
-        //POS - Inserir os usuário
-        
+    if (URL.startsWith("/users")) {  //aqui uso o startsWith que vai pegar tudo que venha do /users em diante
+        //POS - Inserir os usuário        
         if (METHOD === 'POST') {
 
             request.on("data", (data) => {
@@ -28,8 +27,41 @@ const server = http.createServer((request, response) => {
             })
 
         }
+        //GET - Listar todods os usuário
         if (METHOD === 'GET') {
             return response.end(JSON.stringify(users))
+        }
+
+        //PUT - Alterar um usuário
+        if (METHOD === "PUT") {
+            //capturar o ID do usuário para podermos efetuar a alteração
+            const paramSplit = URL.split("/"); //aqui vou dividir a URL que está em postman em / e abaixo só pego a posição 2 do meu array
+            const id = paramSplit[2]
+
+            request.on("data", (data) => {
+                //receber as informaçõe que quero alterar do nosso body
+                const body = JSON.parse(data);
+
+                // identificar qual usuário dentro do array
+                const userIndex = users.findIndex((user) => user.id === id);
+
+                //alterar o usuário  (ID permanece)
+                users[userIndex] = {
+                    ...body,
+                    id,
+                };
+
+                
+            }) 
+            .on("end", () => {
+                //retornar usuário alterado
+                return response.end(
+                    JSON.stringify({
+                        message: "usuário alterado com sucesso!"
+                    })
+                );
+            })
+
         }
 
     }
